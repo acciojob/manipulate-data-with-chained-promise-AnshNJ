260932d37f67099342375360ffc1bddb56d13a57
Ansh Jain <anshjain866@gmail.com>
2024-09-03 23:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const array = [1,2,3,4];
+const div = document.getElementById("output");
+div.innerHTML = array;
+
+function dataHandler() {
+	const data = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			let newArray = array.filter((num) => num%2==0);
+			div.innerHTML = newArray.toString();
+			return resolve(newArray);
+		}, 1000);
+	}).then((newArray) => {
+		setTimeout(() => {
+			newArray = newArray.map((num) => num*2);
+			div.innerHTML = newArray.toString();
+			return resolve(newArray);
+		}, 2000)
+	}).catch((e) => alert(e));
+}
+
+dataHandler();
